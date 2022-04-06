c4d818643e5a43640de8a88d1f6947dc6205dbc8
J-Krinsky <103084199+J-Krinsky@users.noreply.github.com>
2022-04-05 21:43:46 -0400

    Create player_rating.sql
    A query to find the impact of each player across all lineups in an attempt to isolate individual player impacts

---
+++ player_rating.sql
@@ -0,0 +1,26 @@
+#Find overall ratings of indivdual players across lineups
+#Triple join all tables, group by player ID
+SELECT name,
+#Get players Net, offensive defensive ratings based on grouping
+SUM(net)/SUM(poss)*100 as n_rating, SUM(pts)/SUM(poss)*100 AS o_rating, SUM(def)/SUM(poss)*100 AS d_rating
+FROM
+#Use subquery to add total points, defense, net rating and possesions
+(
+    SELECT players.name, players.id AS player_id,
+off_rating*pace*lineups.minutes/4800 AS pts, 
+def_rating*pace*lineups.minutes/4800 AS def, 
+net_rating*pace*lineups.minutes/4800 AS net, 
+pace*lineups.minutes/48 AS poss
+FROM lineups
+JOIN id_index
+ON lineups.id = id_index.lineup_id
+JOIN players
+ON players.id = id_index.player_id_one
+OR players.id = id_index.player_id_two
+OR players.id = id_index.player_id_three
+OR players.id = id_index.player_id_four
+OR players.id = id_index.player_id_five
+) as whop
+GROUP BY player_id
+ORDER BY n_rating DESC
+;
